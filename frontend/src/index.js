f7d82f8fa3a7dfe24d1de6bbcb01e2e200d3d234
umasumathi57 <umasumathi57@gmail.com>
2022-03-03 11:40:03 +0530

    project started

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+//render-("what to show","where to show")
+ReactDom.render(
+    <>
+    <h1>New beginning!</h1>  
+    <p>kumar</p>
+    <h2>kumar</h2>
+    </>
+    ,document.getElementById("root")
+)
