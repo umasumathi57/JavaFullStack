e9fc3d057f1e9c9cda6bab7a3bdafb95f9a36716
umasumathi57 <umasumathi57@gmail.com>
2022-03-06 19:44:50 +0530

    Basic program

--- frontend/src/sample.js
+++ frontend/src/sample.js
@@ -8,6 +8,6 @@ export const Hello=()=>{
     return(<p>*user defined componets 1)Class componets 2)Functional componets</p>)
 }
 
-export const Hi=()=>{
+ export const Hi=()=>{
     return (<p>*for functional components we can create either function method or Fat arrow menthod</p>)
 }
